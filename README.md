ebbe05366c88373386c04cbb5311f237b2514d7f
matt.barr <matt@mysidewalk.com>
2024-01-03 23:08:14 -0600

    Updated performance tests to be easier to repeat under different configs.

--- README.md
+++ README.md
@@ -1,20 +1,55 @@
-Compares 4 algorithms for dot product:
+Compares many algorithms for dot product.
 
-1. Klutzy's naive for loop algorithm
-2. Klutzy's naive for loop with conditional algorithm
-3. marr75's vectorized algorithm
-4. marr75's sparse matrix vectorized algorithm
-5. Klutzy's naive for loop with conditional algorithm (again)
 
-In a sample run for a 10000x10000 matrix:
+In sample runs for a 10000x10000 matrix:
 ```
-Finished release [optimized] target(s) in 0.01s
-Running `target/release/skull`
-Setup duration: 3.175579583s
-Dot product duration: 108.85375ms
-Optimized dot product duration: 89.647875ms
-Dot product loop duration: 66.7235ms
-Optimized dot product loop duration: 32.958334ms
-Vectorized dot product duration: 64.812541ms
-Vectorized SIMD dot product duration: 9.849417ms
+    Finished release [optimized] target(s) in 0.02s
+     Running `target/release/skull`
+
+Running with a uniform distribution around 0 and low sparsity.
+---
+Setup duration: 2.804159875s
+Matrix rank: 10000
+Sparsity of weights matrix: 0.25000973000000004
+Sparsity of activations: 0.5012
+Basic Map: 88.6175ms
+Map with check: 89.340209ms
+Basic loop: 65.801916ms
+Loop with check (CurdledMilk's Implementation): 79.143208ms
+Double checked loop: 175.595542ms
+Vectorized (Array2.dot): 54.310208ms
+Vectorized SIMD: 10.407167ms
+Sparse: 56.656125ms
+
+Running with a uniform distribution around 0.5 and low weights sparsity.
+---
+Setup duration: 2.752812042s
+Matrix rank: 10000
+Sparsity of weights matrix: 0.24995639999999997
+Sparsity of activations: 0.3289
+Basic Map: 87.372708ms
+Map with check: 87.775541ms
+Basic loop: 64.59025ms
+Loop with check (CurdledMilk's Implementation): 43.528125ms
+Double checked loop: 232.548417ms
+Vectorized (Array2.dot): 46.492375ms
+Vectorized SIMD: 9.915083ms
+Sparse: 59.70175ms
+
+Running with a uniform distribution and high weights sparsity.
+---
+Setup duration: 1.336552625s
+Matrix rank: 10000
+Sparsity of weights matrix: 0.99990013
+Sparsity of activations: 0.4978
+Basic Map: 89.014667ms
+Map with check: 89.312625ms
+Basic loop: 66.633459ms
+Loop with check (CurdledMilk's Implementation): 33.896208ms
+Double checked loop: 24.424083ms
+Vectorized (Array2.dot): 54.584542ms
+Vectorized SIMD: 9.965292ms
+Sparse: 73.167µs
+
+Process finished with exit code 0
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-extern crate packed_simd;
-extern crate rayon;
-
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use rand_distr::{Normal, Distribution};
 use rand::{thread_rng, Rng};
 use rand::distributions::Uniform;
@@ -9,6 +6,7 @@ use ndarray::{Array2, Array1};
 use packed_simd::f64x4;
 use packed_simd::f64x8;
 use rayon::prelude::*;
+use sprs::{TriMat, TriMatI, CsMat};
 use std::vec::Vec;
 
 fn initialize_vector_from_distribution<F, T>(n: usize, mut dist_fn: F) -> Vec<T>
@@ -49,7 +47,7 @@ fn optimized_dot_product(vec: &[f64], matrix: &[Vec<f64>]) -> Vec<f64> {
 }
 
 fn dot_product_loop(activation_in: &[f64], weights_matrix: &Vec<Vec<f64>>, n: usize) -> Vec<f64> {
-    let mut activation_out = vec![0.0;n]; //n activations
+    let mut activation_out = vec![0.0; n]; //n activations
     for i in 0..n {
         for j in 0..n {
             activation_out[j] += activation_in[i] * weights_matrix[i][j];
@@ -58,12 +56,26 @@ fn dot_product_loop(activation_in: &[f64], weights_matrix: &Vec<Vec<f64>>, n: us
     return activation_out;
 }
 
-fn optimized_dot_product_loop(activation_in: &Vec<f64>, weights: &Vec<Vec<f64>>, n: usize) -> Vec<f64> {
+fn checked_dot_product_loop(activation_in: &Vec<f64>, weights: &Vec<Vec<f64>>, n: usize) -> Vec<f64> {
     let mut activation_out = vec![0.0; n]; //n activations
     for i in 0..n {
         if activation_in[i] > 0.0 {
             for j in 0..n {
                 activation_out[j] += activation_in[i] * weights[i][j];
+            }
+        }
+    }
+    return activation_out;
+}
+
+fn double_checked_dot_product_loop(activation_in: &Vec<f64>, weights: &Vec<Vec<f64>>, n: usize) -> Vec<f64> {
+    let mut activation_out = vec![0.0; n]; //n activations
+    for i in 0..n {
+        if activation_in[i] > 0.0 {
+            for j in 0..n {
+                if weights[i][j] > 0.0 {
+                    activation_out[j] += activation_in[i] * weights[i][j];
+                }
             }
         }
     }
@@ -99,6 +111,34 @@ fn dot_product_simd(vec: &[f64], matrix: &Vec<Vec<f64>>) -> Vec<f64> {
         .collect()
 }
 
+fn initialize_sparse_vector<F>(size: usize, zero_prob: f64, mut dist_fn: F) -> Vec<f64>
+    where
+        F: FnMut() -> f64,
+{
+    let mut rng = thread_rng();
+    let uniform = Uniform::new(0.0, 1.0);
+    (0..size)
+        .map(|_| {
+            if uniform.sample(&mut rng) < zero_prob {
+                0.0
+            } else {
+                dist_fn()
+            }
+        })
+        .collect()
+}
+
+fn sparse_dot_product(vec: &[f64], matrix: &CsMat<f64>) -> Vec<f64> {
+    let mut result = vec![0.0; matrix.rows()];
+
+    for i in 0..matrix.rows() {
+        for (j, &val) in matrix.outer_view(i).unwrap().iter() {
+            result[j] += vec[i] * val;
+        }
+    }
+
+    result
+}
 
 fn convert_to_array2(matrix: &Vec<Vec<f64>>) -> Array2<f64> {
     let rows = matrix.len();
@@ -109,61 +149,118 @@ fn convert_to_array2(matrix: &Vec<Vec<f64>>) -> Array2<f64> {
     return Array2::from_shape_vec((rows, cols), flattened).unwrap();
 }
 
-fn main() {
-    let start = Instant::now();
-    let n = 10000;
-    let normal = Normal::new(0.0, 1.0).unwrap();
-    let uniform: Uniform<f64> = Uniform::new(-1.0, 1.0);
+fn convert_to_sparse_matrix(dense_matrix: &[Vec<f64>], n: usize) -> CsMat<f64> {
+    // Convert to sparse matrix
+    let mut triplet = TriMat::new((n, n));
+    for (i, row) in dense_matrix.iter().enumerate() {
+        for (j, &val) in row.iter().enumerate() {
+            if val != 0.0 {
+                triplet.add_triplet(i, j, val);
+            }
+        }
+    }
+    return triplet.to_csr();
+}
 
-    let weights_normal = flat_vector_to_matrix(
+fn calculate_sparsity_matrix(matrix: &CsMat<f64>) -> f64 {
+    1.0 - (matrix.nnz() as f64) / ((matrix.rows() * matrix.cols()) as f64)
+}
+
+fn calculate_sparsity_vector(vector: &[f64]) -> f64 {
+    let zero_count = vector.iter().filter(|&&x| x == 0.0).count();
+    zero_count as f64 / vector.len() as f64
+}
+
+fn time_algorithm<F, R>(mut func: F) -> (R, Duration)
+    where
+        F: FnMut() -> R,
+{
+    let start = Instant::now();
+    let result = func();
+    let duration = start.elapsed();
+    (result, duration)
+}
+
+fn run_with_config(
+    n: usize,
+    activations_dist_func: Box<dyn FnMut() -> f64>,
+    weights_dist_func: Box<dyn FnMut() -> f64>,
+    weights_sparsity: f64
+) {
+    let start = Instant::now();
+    let weights = flat_vector_to_matrix(
         n,
-        initialize_vector_from_distribution(n * n, || thread_rng().sample(&normal)),
+        initialize_sparse_vector(n * n, weights_sparsity, weights_dist_func),
     );
-    let weights_uniform = flat_vector_to_matrix(
-        n,
-        initialize_vector_from_distribution(n * n, || thread_rng().sample(&uniform)),
-    );
-    let activation_in = initialize_vector_from_distribution(n, || thread_rng().sample(&normal));
+    let activation_in = initialize_vector_from_distribution(n, activations_dist_func);
     let duration = start.elapsed();
-    let weights = weights_normal;
-    println!("Setup duration: {:?}", duration);
 
-
-    let start = Instant::now();
-    let relu_out = relu(&activation_in);
-    let _dot_product_out = dot_product(&relu_out, &weights);
-    let duration = start.elapsed();
-    println!("Dot product duration: {:?}", duration);
-
-    let start = Instant::now();
-    let relu_out = relu(&activation_in);
-    let _dot_product_out = optimized_dot_product(&relu_out, &weights);
-    let duration = start.elapsed();
-    println!("Optimized dot product duration: {:?}", duration);
-
-    let start = Instant::now();
-    let relu_out = relu(&activation_in);
-    let _dot_product_out = dot_product_loop(&relu_out, &weights, n);
-    let duration = start.elapsed();
-    println!("Dot product loop duration: {:?}", duration);
-
-    let start = Instant::now();
-    let relu_out = relu(&activation_in);
-    let _dot_product_out = optimized_dot_product_loop(&relu_out, &weights, n);
-    let duration = start.elapsed();
-    println!("Optimized dot product loop duration: {:?}", duration);
+    let csr_weights = convert_to_sparse_matrix(&weights, n);
 
     let vectorized_activation_in = Array1::from_vec(activation_in.clone());
     let vectorized_weights_matrix = convert_to_array2(&weights);
-    let start = Instant::now();
-    let relu_out = vectorized_activation_in.mapv(|x| if x < 0.0 { 0.0 } else { x });
-    let _dot_product_out = vectorized_weights_matrix.dot(&relu_out);
-    let duration = start.elapsed();
-    println!("Vectorized dot product duration: {:?}", duration);
 
-    let start = Instant::now();
-    let relu_out = relu(&activation_in);
-    let _dot_product_out = dot_product_simd(&relu_out, &weights);
-    let duration = start.elapsed();
-    println!("Vectorized SIMD dot product duration: {:?}", duration);
+    println!("Setup duration: {:?}", duration);
+    println!("Matrix rank: {:?}", n);
+    println!("Sparsity of weights matrix: {:?}", calculate_sparsity_matrix(&csr_weights));
+    println!("Sparsity of activations: {:?}", calculate_sparsity_vector(&relu(&activation_in)));
+
+
+    let (_dot_product_out, duration) = time_algorithm(|| dot_product(&relu(&activation_in), &weights));
+    println!("Basic Map: {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| optimized_dot_product(&relu(&activation_in), &weights));
+    println!("Map with check: {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| dot_product_loop(&relu(&activation_in), &weights, n));
+    println!("Basic loop: {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| checked_dot_product_loop(&relu(&activation_in), &weights, n));
+    println!("Loop with check (CurdledMilk's Implementation): {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| double_checked_dot_product_loop(&relu(&activation_in), &weights, n));
+    println!("Double checked loop: {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| vectorized_weights_matrix.dot(&vectorized_activation_in.mapv(|x| if x < 0.0 { 0.0 } else { x })));
+    println!("Vectorized (Array2.dot): {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| dot_product_simd(&relu(&activation_in), &weights));
+    println!("Vectorized SIMD: {:?}", duration);
+
+    let (_dot_product_out, duration) = time_algorithm(|| sparse_dot_product(&relu(&activation_in), &csr_weights));
+    println!("Sparse: {:?}", duration);
+}
+
+fn main() {
+    let n = 10000;
+    let normal = Normal::new(0.0, 1.0).unwrap();
+    let shifted_normal = Normal::new(0.5, 1.0).unwrap();
+    let uniform: Uniform<f64> = Uniform::new(-1.0, 1.0);
+    let shifted_uniform = Uniform::new(-0.5, 1.0);
+    let very_sparse = 1.0 - (1.0 / (n as f64));
+    let not_very_sparse = 0.25;
+
+    println!("\nRunning with a uniform distribution around 0 and low sparsity.\n---");
+    run_with_config(
+        10000,
+        Box::new(move || thread_rng().sample(&uniform)),
+        Box::new(move || thread_rng().sample(&uniform)),
+        not_very_sparse,
+    );
+
+    println!("\nRunning with a uniform distribution around 0.5 and low weights sparsity.\n---");
+    run_with_config(
+        10000,
+        Box::new(move || thread_rng().sample(&shifted_uniform)),
+        Box::new(move || thread_rng().sample(&uniform)),
+        not_very_sparse,
+    );
+
+    println!("\nRunning with a uniform distribution and high weights sparsity.\n---");
+    run_with_config(
+        10000,
+        Box::new(move || thread_rng().sample(&uniform)),
+        Box::new(move || thread_rng().sample(&uniform)),
+        very_sparse,
+    );
 }
